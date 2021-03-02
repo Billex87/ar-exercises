af021b2cb59cb3a4e5354a999f861fd8f9794e47
Billy McKinnon <Billy_McKinnon@hotmail.com>
2021-03-01 17:40:39 -0800

    Ex 7 Complete

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates_associated :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, minimum: 0}
+  validate :must_carry_men_or_women_apparel
+
+  def must_carry_men_or_women_apparel
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:apparel, "needs either men's or women's to be true")
+    end
+  end
+
 end
